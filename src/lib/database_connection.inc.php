89fc7e6b1f152f086eb067ac7225a0367c7cbe3e
Espen Andersen <espen@kommunal-rapport.no>
2022-10-05 09:23:05 +0200

    Add PostGIS import routine for GML geodata

--- src/lib/database_connection.inc.php
+++ src/lib/database_connection.inc.php
@@ -1,20 +1,32 @@
 <?php
 
+  require_once( DIR_LIB . '/logger.inc.php' );
+
   class DatabaseConnection {
 
     private $mPdo;
 
     public function __construct( $user, $password, $database, $host, $port ) {
-
+      $pdoOpts = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ];
+      $dsn = "pgsql:host={$host};port={$port};dbname={$database}";
+      $this->mPdo = new PDO( $dsn, $user, $password, $pdoOpts );
     }
 
     public function importPlaces( $placesList ):void {
-      $pdoOpts = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ];
-      $dsn = "pgsql:host={$host};port={$port};dbname={$database}";
-      $pdo = new PDO( $dsn, $user, $password, $pdoOpts );
-      $pdo->prepare( '' );
-
-      $pdo = null;
+      $insert = $this->mPdo->prepare( "INSERT INTO place ( name, pos ) VALUES ( :name, ST_Transform(ST_GeomFromText( :feature, :srid ), 4326 ) )" );
+      $n = count( $placesList );
+      $i = 0;
+      Logger::out( "0 of {$n} geometries stored" );
+      foreach( $placesList as $place ) {
+        $insert->execute( [
+          ':name' => $place[ 0 ],
+          ':srid' => $place[ 1 ],
+          ':feature' => $place[ 2 ] ] );
+        if( ++$i % 1000 == 0 ) {
+          Logger::out( "\r{$i} of {$n} geometries stored          " );
+        }
+      }
+      Logger::out( "\rDone saving {$i} geometries             \n" );
     }
 
   }
